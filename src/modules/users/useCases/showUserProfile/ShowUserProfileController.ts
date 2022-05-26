5dcd26dcf5564ded40194462cb5e6099f5aea521
Guilherme Machado <gmachado@sertrading.lan>
2022-05-26 16:31:36 -0300

    first commit

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const id = request.params.user_id;
+    try {
+      return response.status(200).json(this.showUserProfileUseCase.execute({ user_id: id}));
+    } catch (error) {
+      return response.status(404).json({error: error.message});
+    }
+  
   }
 }
 
